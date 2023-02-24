10eb2af9110f2d5263f72a6e454e07e94fcd7dae
GENADEVICH <116941467+GENADEVICH@users.noreply.github.com>
2023-02-25 01:12:48 +0300

    S1

---
+++ ClientServerSocket/Server/src/Server.java
@@ -0,0 +1,34 @@
+import javaphone.Phone;
+import java.io.*;
+import java.net.ServerSocket;
+
+class Server {
+
+    public static void main(String[] args) throws IOException {
+
+        try (ServerSocket server = new ServerSocket(8000)) {
+            System.out.println("Server started!");
+
+            while (true){
+
+               Phone phone = new Phone (server);
+                    new Thread(() -> {
+                        String request = phone.readLine();
+                        System.out.println("Request: " + request);
+                        String response = (int) (Math.random() * 30 - 10) + "";
+                        try {
+                            Thread.sleep(2000);
+                        } catch (InterruptedException e) {
+                        }
+                        phone.writeLine(response);
+                        System.out.println("Response: " + response);
+                        try {phone.close();} catch (IOException e) { }
+                    }).start();
+                }
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+        }
+    }
+
